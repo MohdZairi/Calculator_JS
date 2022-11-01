d7e931b4d46fbaa709a82ff4805a1e74911793bc
MohdZairi <mohdzairi658@gmail.com>
2022-11-01 09:25:16 +0800

    init

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator JS</title>
+    <link rel = "icon" href ="/Calculator/img/calculator.png">
+    <link rel="stylesheet" href="/Calculator/css/style.css">
+</head>
+<body>
+    <div class="calculator">
+    <div id="display"></div>
+    <table>
+        <tbody>
+        <tr>
+            <td colspan="4">
+            =
+            </td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td>-</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>+</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>*</td>
+        </tr>
+        <tr>
+            <td>0</td>
+            <td>🆑</td>
+            <td>⬅️</td>
+            <td>/</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+</body>
+</html>
